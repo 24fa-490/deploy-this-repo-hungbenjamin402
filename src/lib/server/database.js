d9675199fd8ab8f47235ee15e760cbba3c40a2d0
Manh Hung (Benjamin) Nguyen <hungbenjamin402@gmail.com>
2024-10-27 14:35:05 -0700

    Fix import for PGCONNECT

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,8 +2,8 @@
 import postgres from 'postgres'
 
 // see env variables in .env
-import {PGCONNECT} from '$env/dynamic/private';
+// import {PGCONNECT} from '$env/dymaic/private';
 
-const sql = postgres(PGCONNECT, {} )
+const sql = postgres(process.env.PGCONNECT, {} )
 
 export default sql;
